5de0cfe7badb830c31d4d62c6ae8c7ebf49d6f97
Jannes <jannesventer09@gmail.com>
2020-08-11 15:42:43 +0200

    Added WhatsappAPI

---
+++ KontactKeepers/WhatsappAPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsappAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
